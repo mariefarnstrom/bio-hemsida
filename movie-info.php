2111a979b8162d8ef3884983d55d490821256e85
Marie Färnström <fernstrm@hotmail.com>
2025-11-05 11:24:37 +0100

    added movie-info page with basic structure

---
+++ movie-info.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+
+<h1>
+    <!-- insert camera icon -->
+    <!-- insert movie title -->
+</h1>
+<section class="movieInfo">
+    <div class="coverHolder">
+        <!-- insert movie cover -->
+    </div>
+    <p>
+        <!-- insert movie plot -->
+    </p>
+
+</section>
+<section class="imagesAndTrailer">
+
+
+</section>
+<section class="dealsOfTheWeek">
+    <h2>DEALS OF THE WEEK</h2>
+    <div>
+        <h3>DRINK SPECIAL</h3>
+        <p>STAR JELLY SOUR - $10</p>
+    </div>
+    <div>
+        <h3>FOOD SPECIAL</h3>
+        <p>BOGO POPCORN - $5</p>
+    </div>
+</section>
+<article class="signUp">
+    <h3>SIGN UP FOR OUR NEWSLETTER</h3>
+    <p>ENTER YOUR EMAIL HERE</p>
+</article>
