c9edcf191e281237767c5039e0484a6b868fc3e0
Patricia Loayza Frykberg <patricia_lf_86@hotmail.com>
2025-11-10 20:16:06 +0100

    Added :root and body in style.css

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Futurity</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="movie-info.css">
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -9,29 +9,33 @@ require __DIR__ . '/header.php';
 
 <section class="showingNow">
     <h2>NOW SHOWING <!-- insert camera icon --> </h2>
-    <section class="nowShowingCarousel">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-    </section>
+    <a href="movie-info.php">
+        <section class="nowShowingCarousel">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </section>
+    </a>
 </section>
 <section class="dealsSection">
     <article class="classicOfTheMonth">
         <h2>CLASSIC OF THE MONTH</h2>
+        <a href="movie-info.php">
             <div>
                 <h3>TERMINATOR 2 - JUDGMENT DAY (1991)</h3>
                 <!-- insert classic movie cover -->
             </div>
+        </a>
     </article>
     <article class="deals">
         <h2>DEALS OF THE WEEK</h2>
         <div>
-        <div>
-            <img src="images/octopus-glass 1.png">
-        </div>
+            <div>
+                <img src="images/octopus-glass 1.png">
+            </div>
             <h3>DRINK SPECIAL</h3>
             <p>STAR JELLY SOUR - $10</p>
         </div>
@@ -44,14 +48,16 @@ require __DIR__ . '/header.php';
 
 <section class="comingSoon">
     <h2>COMING SOON</h2>
-    <section class="comingSoonCarousel">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-    </section>
+    <a href="movie-info.php">
+        <section class="comingSoonCarousel">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </section>
+    </a>
 </section>
 
 <article class="signUp">
