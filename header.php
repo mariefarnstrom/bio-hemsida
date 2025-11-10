723d922304c01189127a92da25d73ce8d409eeab
Elsa Girardo <egirardo@hotmail.com>
2025-11-10 12:07:13 +0100

    Updated Image folder and moved image files into respective folders. Updated image tags in header to reflect

--- header.php
+++ header.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Futurity</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <header>
         <img src="images/futurity-logo.png">
@@ -13,7 +15,7 @@
             <a href="">HOME</a>
             <a href="">THEATERS</a>
             <a href="">SHOWTIME</a>
-            <a href=""><img src="images/search.png"></a>
-            <a href=""><img src="images/alien-icon 1.png"></a>
+            <a href=""><img src="images/icons/search.png"></a>
+            <a href=""><img src="images/icons/alien-icon.png"></a>
         </nav>
     </header>
