9179b0a0f72ac882621b23734a2eff0bb3c9ae4e
Elsa Girardo <egirardo@hotmail.com>
2025-11-12 11:53:24 +0100

    Feature: Saving progress on film carousel. REM conversion still in progress. Corrections still needed for positioning of carousel as well.

--- elsa-test.php
+++ elsa-test.php
@@ -1,3 +1,41 @@
+<?php
+$tealFrame = '<svg class="teal-frame" xmlns="http://www.w3.org/2000/svg" width="284" height="399" viewBox="0 0 284 399" fill="none">
+        <g filter="url(#filter0_d_2192_6)">
+            <path d="M258.471 385.137H18.1578C15.5188 385.137 13.3795 382.998 13.3795 380.359V18.1578C13.3795 15.5188 15.5188 13.3794 18.1578 13.3794H265.678C268.317 13.3794 270.456 15.5188 270.456 18.1578V372.959" stroke="var(--turquoise)" stroke-opacity="0.89" stroke-width="1.91135" stroke-linecap="round" stroke-linejoin="round" shape-rendering="crispEdges" />
+        </g>
+        <defs>
+            <filter id="filter0_d_2192_6" x="0" y="-7.62939e-06" width="283.835" height="398.516" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                <feOffset />
+                <feGaussianBlur stdDeviation="6.21189" />
+                <feComposite in2="hardAlpha" operator="out" />
+                <feColorMatrix type="matrix" values="0 0 0 0 0.386218 0 0 0 0 0.979541 0 0 0 0 1 0 0 0 0.89 0" />
+                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2192_6" />
+                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2192_6" result="shape" />
+            </filter>
+        </defs>
+    </svg>';
+
+$pinkFrame = '<svg xmlns="http://www.w3.org/2000/svg" width="284" height="399" viewBox="0 0 284 399" fill="none">
+  <g filter="url(#filter0_d_2192_3)">
+    <path d="M258.471 385.137H18.1578C15.5188 385.137 13.3795 382.998 13.3795 380.359V18.1578C13.3795 15.5188 15.5188 13.3794 18.1578 13.3794H265.678C268.317 13.3794 270.456 15.5188 270.456 18.1578V372.959" stroke="var(--pink)" stroke-width="1.91135" stroke-linecap="round" stroke-linejoin="round"/>
+  </g>
+  <defs>
+    <filter id="filter0_d_2192_3" x="-7.62939e-06" y="-7.62939e-06" width="283.835" height="398.516" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+      <feOffset/>
+      <feGaussianBlur stdDeviation="6.21189"/>
+      <feComposite in2="hardAlpha" operator="out"/>
+      <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 0.180082 0 0 0 0 0.904343 0 0 0 1 0"/>
+      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2192_3"/>
+      <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2192_3" result="shape"/>
+    </filter>
+  </defs>
+</svg>';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -28,24 +66,7 @@
     <div class="hero">
         <img class="hero-text" src="images/hero-text.png" alt="Your local theater showcasing both new and classic films">
     </div>
-
-    <!-- <section class="showingNow">
-    <h2>NOW SHOWING</h2>
-    <a href="movie-info.php">
-        <section class="nowShowingCarousel">
-            <div>
-                <img src="/images/movie-posters/Ash.jpg" alt="Ash">
-                <p>Ash</p>
-            </div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </section>
-    </a>
-</section> -->
-    <div class="now-showing">
+    <section class="now-showing">
         <div class="now-showing-title-container">
             <div class="title-container">
                 <div class="title">NOW SHOWING</div>
@@ -56,67 +77,50 @@
         <div class="film-container">
             <div class="film-carousel">
                 <div class="teal-frame-movie">
-                    <img class="teal-image-icon" src="images/movie-posters/Ash.jpg" alt="movie poster for Ash">
+                    <img class="teal-image-icon" src="images/movie-posters/Ash.jpg" alt="Ash poster">
 
-                    <svg class="teal-frame" xmlns="http://www.w3.org/2000/svg" width="284" height="399" viewBox="0 0 284 399" fill="none">
-                        <g filter="url(#filter0_d_2192_6)">
-                            <path d="M258.471 385.137H18.1578C15.5188 385.137 13.3795 382.998 13.3795 380.359V18.1578C13.3795 15.5188 15.5188 13.3794 18.1578 13.3794H265.678C268.317 13.3794 270.456 15.5188 270.456 18.1578V372.959" stroke="#62FAFF" stroke-opacity="0.89" stroke-width="1.91135" stroke-linecap="round" stroke-linejoin="round" shape-rendering="crispEdges" />
-                        </g>
-                        <defs>
-                            <filter id="filter0_d_2192_6" x="0" y="-7.62939e-06" width="283.835" height="398.516" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                                <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
-                                <feOffset />
-                                <feGaussianBlur stdDeviation="6.21189" />
-                                <feComposite in2="hardAlpha" operator="out" />
-                                <feColorMatrix type="matrix" values="0 0 0 0 0.386218 0 0 0 0 0.979541 0 0 0 0 1 0 0 0 0.89 0" />
-                                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_2192_6" />
-                                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_2192_6" result="shape" />
-                            </filter>
-                        </defs>
-                    </svg>
+                    <?= $tealFrame ?>
 
                 </div>
                 <div class="pink-frame-movie">
-                    <img class="pink-image-icon" alt="">
+                    <img class="pink-image-icon" src="/images/movie-posters/mickey17.jpg" alt="Mickey 17 poster">
 
-                    <img class="pink-frame" alt="">
+                    <?= $pinkFrame; ?>
 
                 </div>
                 <div class="teal-frame-movie">
-                    <img class="image-icon" alt="">
+                    <img class="teal-image-icon" src="/images/movie-posters/star-trek-section-31.jpg" alt="Star Trek: Section 31 poster">
 
-                    <img class="teal-frame-movie-child" alt="">
+                    <?= $tealFrame; ?>
 
                 </div>
                 <div class="pink-frame-movie">
-                    <img class="now-showing-image-icon" alt="">
+                    <img class="pink-image-icon" src="/images/movie-posters/tron-ares.jpg" alt="Tron: Ares">
 
-                    <img class="teal-frame-movie-child" alt="">
+                    <?= $pinkFrame; ?>
 
                 </div>
                 <div class="teal-frame-movie">
-                    <img class="image-icon" alt="">
+                    <img class="teal-image-icon" src="/images/movie-posters/predator-badlands.jpeg" alt="Predator: Badlands poster">
 
-                    <img class="teal-frame-movie-child" alt="">
+                    <?= $tealFrame; ?>
 
                 </div>
                 <div class="pink-frame-movie">
-                    <img class="now-showing-image-icon" alt="">
+                    <img class="pink-image-icon" src="/images/movie-posters/companion.jpg" alt="Companion poster">
 
-                    <img class="teal-frame-movie-child" alt="">
+                    <?= $pinkFrame; ?>
 
                 </div>
             </div>
         </div>
-    </div>
+    </section>
     <section class="dealsSection">
         <article class="classicOfTheMonth">
             <h2>CLASSIC OF THE MONTH</h2>
             <a href="movie-info.php">
                 <div>
                     <h3>TERMINATOR 2 - JUDGMENT DAY (1991)</h3>
-                    <!-- insert classic movie cover -->
                 </div>
             </a>
         </article>
