90fd8843e9328d538c2af2fe4dd0775d6d922677
Elsa Girardo <egirardo@hotmail.com>
2025-11-12 13:56:41 +0100

    Feature: Added footer and footer styling to index page

--- footer.php
+++ footer.php
@@ -1,7 +1,12 @@
 <footer>
-    <p>YOU'LL BE BACK</p>
-    <p>&copy 2025 FUTURITY</p>
-    <!-- INSERT SOCIAL MEDIA ICONS -->
+    <div class="youll-be-back">YOU’LL BE BACK!</div>
+    <div class="futurity">© 2025 FUTURITY</div>
+    <div class="socials">
+        <img class="insta-icon" src="/images/icons/insta-icon.png" alt="Instagram icon outline">
+        <img class="facebook-icon" src="/images/icons/facebook-icon.png" alt="Facebook icon outline">
+        <img class="youtube-icon" src="/images/icons/youtube-icon.png" alt="Youtube icon outline">
+    </div>
 </footer>
 </body>
+
 </html>
