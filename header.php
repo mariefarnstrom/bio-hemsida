3ae743055fc6431a060383f6378baa063692fbcb
Elsa Girardo <egirardo@hotmail.com>
2025-11-26 10:45:02 +0100

    Reapply "added hamburgermenu"
    This reverts commit 23731f636f2f9ffd328b21ad695e11bbc24366a9.

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Text+Me+One&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="movie-info.css">
     <link rel="stylesheet" href="media-queries.css">
 </head>
@@ -16,11 +17,35 @@
 <body>
     <header>
         <img class="futurity-logo" src="images/futurity-logo.png" alt="futurity">
+
+        <!-- Hamburger-knapp (mobil) -->
+        <button class="hamburger" onclick="toggleMenu()" aria-label="Toggle menu">
+            <img src="images/icons/menu.svg" alt="" class="hamburger-icon">
+            <img src="images/icons/cancel.svg" alt="" class="close-icon">
+        </button>
+
+        <!-- Desktop meny -->
         <div class="header-links">
-            <a class="home">HOME</a>
-            <a class="theaters">THEATERS</a>
-            <a class="showtimes">SHOWTIMES</a>
-            <img class="search-icon" src="images/icons/search.png" alt="magnifying glass">
-            <img class="alien-icon" src="images/icons/alien-icon.png" alt="outline of an alien">
+            <a href="/index.php" class="home">HOME</a>
+            <a href="#theaters" class="theaters">THEATERS</a>
+            <a href="#showtimes" class="showtimes">SHOWTIMES</a>
+            <img class="search-icon" src="images/icons/search.png" alt="Search">
+            <img class="alien-icon" src="images/icons/alien-icon.png" alt="Profile">
         </div>
     </header>
+
+    <!-- Mobil meny (overlay) -->
+    <div class="mobile-menu">
+        <div class="mobile-menu-content">
+            <img class="alien-icon-large" src="images/icons/alien-icon.png" alt="Profile">
+
+            <a href="/index.php">HOME</a>
+            <a href="#theaters">THEATERS</a>
+            <a href="#showtimes">SHOWTIMES</a>
+
+            <div class="search-box">
+                <img src="images/icons/search.png" alt="">
+                <input type="text" placeholder="Search">
+            </div>
+        </div>
+    </div>
