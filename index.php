003ed9437f05d7814c46b6f0934f98fdc2e9b5e8
Marie Färnström <fernstrm@hotmail.com>
2025-11-28 15:31:49 +0100

    added film titles in carousels

--- index.php
+++ index.php
@@ -20,41 +20,59 @@ require __DIR__ . '/variables.php';
     </div>
     <div class="film-container">
         <div class="film-carousel">
+            <div>
             <div class="teal-frame-movie">
                 <img class="teal-image-icon" src="images/movie-posters/Ash.jpg" alt="Ash poster">
 
                 <?= $tealFrame ?>
 
             </div>
+            <h4>Ash</h4>
+            </div>
+            <div>
             <div class="pink-frame-movie">
                 <img class="pink-image-icon" src="/images/movie-posters/mickey17.jpg" alt="Mickey 17 poster">
 
                 <?= $pinkFrame; ?>
 
             </div>
+            <h4>Mickey 17</h4>
+            </div>
+            <div>
             <div class="teal-frame-movie">
                 <img class="teal-image-icon" src="/images/movie-posters/star-trek-section-31.jpg" alt="Star Trek: Section 31 poster">
 
                 <?= $tealFrame; ?>
 
             </div>
+            <h4>Star trek: Section 31</h4>
+            </div>
+            <div>
             <div class="pink-frame-movie">
                 <img class="pink-image-icon" src="/images/movie-posters/tron-ares.jpg" alt="Tron: Ares poster">
 
                 <?= $pinkFrame; ?>
 
             </div>
+            <h4>Tron: Ares</h4>
+            </div>
+            <div>
             <div class="teal-frame-movie">
                 <img class="teal-image-icon" src="/images/movie-posters/predator-badlands.jpeg" alt="Predator: Badlands poster">
 
                 <?= $tealFrame; ?>
 
             </div>
+            <h4>Predator: Badlands</h4>
+            </div>
+            <div>
             <div class="pink-frame-movie">
                 <img class="pink-image-icon" src="/images/movie-posters/companion.jpg" alt="Companion poster">
 
                 <?= $pinkFrame; ?>
 
+            </div>
+            <h4>Companion</h4>
             </div>
         </div>
     </div>
@@ -101,41 +119,59 @@ require __DIR__ . '/variables.php';
     </div>
     <div class="film-container">
         <div class="film-carousel">
+            <div>
             <div class="teal-frame-movie">
                 <img class="teal-image-icon" src="images/movie-posters/avatar-fireandash.jpeg" alt="Avatar: Fire and Ash poster">
 
                 <?= $tealFrame ?>
 
             </div>
+            <h4>Avatar: Fire and ash</h4>
+            </div>
+            <div>
             <div class="pink-frame-movie">
                 <img class="pink-image-icon" src="/images/movie-posters/the-dog-stars.jpg" alt="The Dog Stars poster">
 
                 <?= $pinkFrame; ?>
 
             </div>
+            <h4>The dog stars</h4>
+            </div>
+            <div>
             <div class="teal-frame-movie">
                 <img class="teal-image-icon" src="/images/movie-posters/project-hail-mary.jpg" alt="Project Hail Mary poster">
 
                 <?= $tealFrame; ?>
 
             </div>
+            <h4>Project hail Mary</h4>
+            </div>
+            <div>
             <div class="pink-frame-movie">
                 <img class="pink-image-icon" src="/images/movie-posters/dune-part-three.jpg" alt="Dune Part Three">
 
                 <?= $pinkFrame; ?>
 
             </div>
+            <h4>Dune: Part three</h4>
+            </div>
+            <div>
             <div class="teal-frame-movie">
                 <img class="teal-image-icon" src="/images/movie-posters/the-mandalorian-and-grogu.jpg" alt="Star Wars: The Mandalorian and Grogu poster">
 
                 <?= $tealFrame; ?>
 
             </div>
+            <h4>Star Wars: The Mandalorian and Grogu</h4>
+            </div>
+            <div>
             <div class="pink-frame-movie">
                 <img class="pink-image-icon" src="/images/movie-posters/28yearslater.jpg" alt="28 Years Later poster">
 
                 <?= $pinkFrame; ?>
 
+            </div>
+            <h4>28 years later</h4>
             </div>
         </div>
     </div>
